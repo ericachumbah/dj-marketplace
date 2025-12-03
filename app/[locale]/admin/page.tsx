6baa22578b813aa787e6888f0cfec1f8a65e5b9e
mbende sampson <mbende2000@yahoo.com>
2025-12-03 19:03:23 +0100

    fix: Skip static generation for admin page

--- app/[locale]/admin/page.tsx
+++ app/[locale]/admin/page.tsx
@@ -1,5 +1,9 @@
 import AdminDashboard from "@/app/components/admin/Dashboard";
 
+export function generateStaticParams() {
+  return [];
+}
+
 export default function AdminPage() {
   return <AdminDashboard />;
 }
